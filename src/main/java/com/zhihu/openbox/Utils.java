b85fa6f289c778d750f03079ebd38d25a50f1ae9
shiluming <00243050172qq>
2018-04-04 21:35:41 +0800

    修改密码生成规则，保证生成的密码有数字和密码

--- src/main/java/com/zhihu/openbox/Utils.java
+++ src/main/java/com/zhihu/openbox/Utils.java
@@ -20,11 +20,19 @@ public class Utils {
 
     private Utils(){};
 
-    private static final char[] PASSWORD_SEED = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'j',
+    /**
+     * char密码
+     */
+    private static final char[] PASSWORD_CHAR_SEED = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'j',
     'i', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
     '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G',
     'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
     'Y', 'Z'};
+
+    /**
+     * 数字密码
+     */
+    private static final char[] PASSWORD_NUM_SEED = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 
     /**
      * 随机生成固定长度的密码
@@ -35,8 +43,17 @@ public class Utils {
         StringBuilder sb = new StringBuilder();
         Random random = new Random();
         for (int i = 0; i < number; i++) {
-            int index = random.nextInt(PASSWORD_SEED.length);
-            sb.append(PASSWORD_SEED[index]);
+            int index = random.nextInt(PASSWORD_CHAR_SEED.length);
+            sb.append(PASSWORD_CHAR_SEED[index]);
+        }
+        boolean matches = sb.toString().matches(".*\\d+.*");
+        if (!matches) {
+            System.out.println("替换前：" + sb.toString());
+            int i1 = random.nextInt(PASSWORD_NUM_SEED.length);
+            sb.setCharAt(8, PASSWORD_NUM_SEED[i1]);
+            int i2 = random.nextInt(PASSWORD_NUM_SEED.length);
+            sb.setCharAt(9, PASSWORD_NUM_SEED[i2]);
+            System.out.println("替换后：" + sb.toString());
         }
         return sb.toString();
     }
@@ -68,7 +85,7 @@ public class Utils {
             createFile(name);
         }
         OutputStreamWriter writer = new FileWriter(file, true);
-        writer.append(phoneNo + "\t" + password + "\r\n");
+        writer.append(phoneNo + "------------" + password + "\r\n");
         writer.flush();
         writer.close();
         return true;
